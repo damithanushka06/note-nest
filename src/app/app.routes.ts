82ec76608d0b243e956e3c5f3f136cfb7443e085
Damith Anushka <155383770+damithanushka06@users.noreply.github.com>
2025-05-28 19:43:04 +0530

    Added basic components to setup main structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {NoteListComponent} from './features/notes/note-list/note-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NoteListComponent },
+];
